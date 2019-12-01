6e8ca5a8c7cc2b9e0994ebb53ed5ece23382ceda
ilyat <ilya.tretyakov@velaskec.com>
2019-12-01 14:49:05 +0300

    Solve MVC Issue

---
+++ TG.Exam.WebMVC/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salestech.Exam.WebMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = TG.Exam.WebMVC.Models.User.GetAll();
+            return View(users);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ TG.Exam.WebMVC/Controllers/UserOperationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TG.Exam.WebMVC.Models;
+
+namespace Salestech.Exam.WebMVC.Controllers
+{
+    public class UserOperationController : ApiController
+    {
+        // GET: api/UserOperation
+        public IEnumerable<User> GetAllUsers()
+        {
+            return TG.Exam.WebMVC.Models.User.GetAll();
+        }
+    }
+}
